a140cb334d77a488ed23c2f8734816b2aef57485
Travis <travis.williams94@gmail.com>
2023-05-29 17:19:53 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    
+  ];
+
+
+
+function superbowlWin(record){
+    for (let i=0; i < record.length; i++){
+    if (record[i].result === "W"){
+        return record[i].year;
+    }}
+};
+
+record.find(superbowlWin);
